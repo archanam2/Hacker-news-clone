890126a0057c4bc6e6ed86c9866a2a19b433700f
archanam2 <maliarchanabm@gmail.com>
2020-11-29 11:24:38 +0530

    Done with responsive

---
+++ src/components/End.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+const End = () => {
+  return (
+  <footer>
+      <p>
+          @ Hacker News Clone | | All rights are reserved....
+      </p>
+      <ul>
+      <li>
+                <a
+                  id="icon"
+                  href="https://github.com/archanam2"
+                  target="_blank"
+                  ><i class="fab fa-github fa-2x"></i
+                ></a>
+              </li>
+              
+              <li>
+                <a
+                  id="icon"
+                  href="https://www.linkedin.com/in/ArchanaMali/"
+                  target="_blank"
+                  ><i class="fab fa-linkedin-in fa-2x"></i
+                ></a>
+              </li>
+
+                <li>
+                <a id="icon" href="mailto:maliarchanabm@gmail.com" target="_blank"
+                  ><i class="fab fa-google fa-2x"></i
+                ></a>
+              </li>
+
+        </ul>
+  </footer>
+  );
+};
+
+export default End;
